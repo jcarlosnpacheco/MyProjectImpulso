61d78d506057e6d5ab2eb363a26a4bfd1ce3f1d9
Joao <jcarlosnpacheco@gmail.com>
2022-06-03 14:42:10 -0400

    Created entity Candidadate

---
+++ 7.1.0/aspnet-core/src/MyProjectImpulso.Core/Candidates/Candidate.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyProjectImpulso.Candidates
+{
+    [Table("Candidates")]
+    public class Candidate : FullAuditedEntity<int>
+    {
+        public string Name { get; set; }
+    }
+}
