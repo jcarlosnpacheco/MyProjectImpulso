bafeb0d5400033b8a07867b08d4c348d82833d55
Joao <jcarlosnpacheco@gmail.com>
2022-06-03 15:42:41 -0400

    AbpAuthorize class CandidateAppService

--- 7.1.0/aspnet-core/src/MyProjectImpulso.Application/Candidates/CandidateAppService.cs
+++ 7.1.0/aspnet-core/src/MyProjectImpulso.Application/Candidates/CandidateAppService.cs
@@ -1,10 +1,12 @@
 ﻿using Abp.Application.Services;
 using Abp.Application.Services.Dto;
+using Abp.Authorization;
 using Abp.Domain.Repositories;
 using MyProjectImpulso.Candidates.Dto;
 
 namespace MyProjectImpulso.Candidates
 {
+    [AbpAuthorize]
     public class CandidateAppService : AsyncCrudAppService<Candidate,CandidateDto,int, PagedResultRequestDto>, ICandidateAppService
     {
         public CandidateAppService(IRepository<Candidate, int> candidateTypeRepository)
