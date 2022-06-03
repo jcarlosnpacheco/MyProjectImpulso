e3a68a5afd00d9c2caf012084dc76e4694456e92
Joao <jcarlosnpacheco@gmail.com>
2022-06-03 14:45:35 -0400

    created dbSet for candidates

--- 7.1.0/aspnet-core/src/MyProjectImpulso.EntityFrameworkCore/EntityFrameworkCore/MyProjectImpulsoDbContext.cs
+++ 7.1.0/aspnet-core/src/MyProjectImpulso.EntityFrameworkCore/EntityFrameworkCore/MyProjectImpulsoDbContext.cs
@@ -3,12 +3,14 @@ using Abp.Zero.EntityFrameworkCore;
 using MyProjectImpulso.Authorization.Roles;
 using MyProjectImpulso.Authorization.Users;
 using MyProjectImpulso.MultiTenancy;
+using MyProjectImpulso.Candidates;
 
 namespace MyProjectImpulso.EntityFrameworkCore
 {
     public class MyProjectImpulsoDbContext : AbpZeroDbContext<Tenant, Role, User, MyProjectImpulsoDbContext>
     {
         /* Define a DbSet for each entity of the application */
+        public DbSet<Candidate> Candidates { get; set; }
         
         public MyProjectImpulsoDbContext(DbContextOptions<MyProjectImpulsoDbContext> options)
             : base(options)
