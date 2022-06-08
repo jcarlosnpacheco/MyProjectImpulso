d46751d28e942a184b584763187c91f843f1c3f3
Joao <jcarlosnpacheco@gmail.com>
2022-06-08 17:19:14 -0400

    created service candidates

--- angular/src/app/candidates/candidates/candidates.component.ts
+++ angular/src/app/candidates/candidates/candidates.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { Component, Injector } from "@angular/core";
+import { appModuleAnimation } from "@shared/animations/routerTransition";
+import {
+  PagedListingComponentBase,
+  PagedRequestDto,
+} from "@shared/paged-listing-component-base";
+import { CandidateDto, CandidateDtoPagedResultDto, CandidateServiceProxy } from "@shared/service-proxies/service-proxies";
+import { finalize } from "rxjs/operators";
 
 @Component({
-  templateUrl: './candidates.component.html',
-  animations: [appModuleAnimation()]
+  templateUrl: "./candidates.component.html",
+  animations: [appModuleAnimation()],
 })
-export class CandidatesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class CandidatesComponent extends PagedListingComponentBase<CandidateDto> {
+  candidates: CandidateDto[] = [];
+  constructor(injector: Injector, private _candidateService: CandidateServiceProxy) {
+    super(injector);
   }
 
+  protected list(
+    request: PagedRequestDto,
+    pageNumber: number,
+    finishedCallback: Function
+  ): void {
+    this._candidateService
+      .getAll(
+        request.skipCount,
+        request.maxResultCount
+      )
+      .pipe(
+        finalize(() => {
+          finishedCallback();
+        })
+      )
+      .subscribe((result: CandidateDtoPagedResultDto) => {
+        this.candidates = result.items;
+        this.showPaging(result, pageNumber);
+      });
+  }
+  protected delete(entity: CandidateDto): void {
+    throw new Error("Method not implemented.");
+  }
 }
